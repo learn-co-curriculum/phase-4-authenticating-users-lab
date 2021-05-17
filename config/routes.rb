104455092f1b24a2f1e093511af077ff5ab2deea
ihollander <ianholla@gmail.com>
2021-05-17 09:41:33 -0400

    Added solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get "/me", to: "users#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
