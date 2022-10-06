ee36fa8a8d85f3e480bbbaee945ebf42fe3dd48d
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-06 13:44:25 +0300

    commit tree f259fec4657f1dd4e16a2e93d2b0e211e13757af

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  get "/me", to: "users#show"
+
 end
